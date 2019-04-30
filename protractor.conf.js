893463560024764de0204cc33e0dbb9670e254e3
jihene-1995 <jihene37@hotmail.fr>
2019-04-30 14:59:36 +0100

    change_args

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,7 +5,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ['--no-sandbox']
+      args: ['--disable-gpu', '--no-sandbox', '--disable-extensions', '--disable-dev-shm-usage']
     }
   },
   directConnect: true,
